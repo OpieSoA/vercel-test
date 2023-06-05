2715d4200cff0a2e777c6f27f28746450bdccc21
-Kevin_ <74331400+OpieSoA@users.noreply.github.com>
2023-06-05 14:42:56 -0300

    test

---
+++ js/test.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.info-cards', { delay: 700 });
+ScrollReveal().reveal('.info-cards1', { delay: 700 });
+ScrollReveal().reveal('.cards-1060', { delay: 700 });
